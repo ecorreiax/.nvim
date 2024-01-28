a3d545645ce91c604d78cd2f64f26b3c26fa771b
Eduardo Correia <ecorreiax@gmail.com>
2024-01-28 02:09:34 +0000

    fix code snippets

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -1,11 +1,16 @@
 return {
     {
         "L3MON4D3/LuaSnip",
+        dependencies = { "rafamadriz/friendly-snippets" },
         -- follow latest release.
         version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
         -- install jsregexp (optional!).
         build = "make install_jsregexp",
 
-        require("luasnip.loaders.from_vscode").lazy_load(),
+        config = function ()
+            require("luasnip.loaders.from_vscode").load({include = {"html"}})
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end
+
     }
 }
