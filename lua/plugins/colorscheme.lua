c18989ddd7f0787315f40e5a0d50cd1846d7e2fc
Eduardo Correia <ecorreiax@gmail.com>
2024-05-04 15:59:15 -0300

    change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,19 @@
 return {
 	{
-		"Mofiqul/vscode.nvim",
+		"metalelf0/jellybeans-nvim",
 		priority = 1000,
-		config = function()
-			require("vscode").setup({})
-
-			vim.cmd("colorscheme vscode")
+		dependencies = {
+			"rktjmp/lush.nvim",
+		},
+		init = function()
+			vim.cmd("colorscheme jellybeans-nvim")
 
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		end,
+		-- config = function()
+		-- 	-- require("vscode").setup({})
+		--
+		-- end,
 	},
 }
